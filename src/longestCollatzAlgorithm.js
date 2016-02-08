2fd128d87053f94014b151b8407b4cd9e3855035
Kevin  Washington <kevw12188@gmail.com>
2016-02-08 07:57:14 -0500

    initial commit

---
+++ src/longestCollatzAlgorithm.js
@@ -0,0 +1,29 @@
+export default class LongestCollatzAlgorithm {
+    constructor(max) {
+        this.MAX_NUMBER = max
+        this.int_under_test = ""
+        this.current_collatz_by_product = ""
+        this.collatz_chain_counts = []
+        this.testing_working_msg = "working"
+    }
+
+    main() {
+
+    }
+
+    collatzChainCountKnown(intToMatch) {
+
+    }
+
+    collatz(int) {
+
+    }
+
+    complete(int) {
+
+    }
+
+    confirmTestsWork() {
+        return this.testing_working_msg
+    }
+}
---
+++ test/longestCollatzAlgorithm.spec.js
@@ -0,0 +1,21 @@
+import LongestCollatzAlgorithm from "src/longestCollatzAlgorithm"
+
+describe("CollatzAlgorithm", () => {
+    describe("main", () => {
+
+    })
+    describe("collatzChainCountKnown", () => {
+
+    })
+    describe("collatz", () => {
+
+    })
+    describe("complete", () => {
+
+    })
+    describe("confirmTestsWork", () => {
+        const MAX = 10
+        const classUnderTest = new LongestCollatzAlgorithm(MAX)
+        
+    })
+})
